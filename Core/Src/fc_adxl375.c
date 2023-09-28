da9be86dc1251893f6563baaec00a3094d49f7d7
devJunchenWu <junchenwu05@gmail.com>
2023-09-27 21:00:30 -0400

    Started adding the function implemention for the Write function (code incomplete currently)

--- Core/Src/fc_adxl375.c
+++ Core/Src/fc_adxl375.c
@@ -6,3 +6,7 @@
  */
 
 #include "fc_adxl375.h"
+
+HAL_StatusTypeDef fc_adxl375_writeregister(struct fc_adxl375 *device, uint8_t reg, uint8_t *data) {
+	return HAL_I2C_Mem_Write(device->i2c_handle, FC_ADXL375_I2C_DEVICE_ID, reg, sizeof(reg), data, 1, 100);
+}
