f91914d13853b70fcad19d01c837a3c7a9492bc1
devJunchenWu <junchenwu05@gmail.com>
2023-10-08 14:16:50 -0400

    Initialize struct variables

--- Core/Src/fc_adxl375.c
+++ Core/Src/fc_adxl375.c
@@ -10,6 +10,14 @@
 
 uint8_t adxl375_initialize(struct fc_adxl375 *device, I2C_HandleTypeDef *i2c_Handle) {
 
+	/* Set struct parameters */
+	dev->i2c_handle		= i2c_handle;
+
+	dev->acceleration_x = 0.0f;
+	dev->acceleration_y = 0.0f;
+	dev->acceleration_z = 0.0f;
+
+	dev->temperature    = 0.0f;
 }
 
 HAL_StatusTypeDef fc_adxl375_readregister(struct fc_adxl375 *device, uint8_t reg, uint8_t *data) {
